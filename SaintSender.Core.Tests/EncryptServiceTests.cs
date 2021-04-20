bae8b0ff78fd01e88792f38a7e8688d76eb27527
Mate Szathmari <mateszathmari@gmail.com>
2021-04-20 11:39:03 +0200

    Add test to Encryption service

---
+++ SaintSender.Core.Tests/EncryptServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using SaintSender.Core.Services;
+
+namespace SaintSender.Core.Tests
+{
+    [TestFixture]
+    public class EncryptServiceTests
+    {
+
+        [Test]
+        public void Encrypt_text_TextShouldBeTheSameAfterEncryptionAndDecryption()
+        {
+            //Arrange
+            EncryptService encryptService = new EncryptService();
+            string textToTest = "This is a test text what we should encrypt and decrypt";
+            //Act
+           string encryptedText = encryptService.Encrypt(textToTest);
+           string decryptedText = encryptService.Decrypt(encryptedText);
+            //Assert
+            Assert.AreEqual(textToTest,decryptedText);
+        }
+
+    }
+}
