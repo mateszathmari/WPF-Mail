2e2db00940f233a434efadcd1bbabee5aea820b7
Mate Szathmari <mateszathmari@gmail.com>
2021-04-20 08:34:33 +0200

    Add test for get mails

--- SaintSender.Core.Tests/MailServiceTests.cs
+++ SaintSender.Core.Tests/MailServiceTests.cs
@@ -1,23 +1,44 @@
-﻿using NUnit.Framework;
-using NUnit.Framework.Internal;
-using SaintSender.Core.Services;
+﻿using System.Collections.Generic;
+using MailKit;
+using NSubstitute;
+using NUnit.Framework;
+using SaintSender.Core.Models;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailService = SaintSender.Core.Services.MailService;
 
 namespace SaintSender.Core.Tests
 {
     [TestFixture]
     public class MailServiceTests
     {
-        [Test]
-        public void CheckInternet_ShouldReturnTrue()
-        {
-            //bool isNetAvailable = MailService.CheckInternet();
+        MailService service = new MailService();
+        private ImapClient _client;
 
+        [Test]
+        public void GetMails_CorrectCredentialsAdded_ReturnEmails()
+        {
             // Arrange
-            var service = new GreetService();
+            
             // Act
-            var greeting = service.Greet(".NET Padawan");
+            List<Email> emails = service.GetMails("tom1.wales2@gmail.com", "Almafa1234");
             // Assert
-            Assert.AreEqual("Welcome .NET Padawan, my friend!", greeting);
+            Assert.True(emails.Count != 0);
+        }
+
+        [Test]
+        public void AddEmailsToList_AddNewMail_ReturnEmailsWithNewMail()
+        {
+            // Arrange
+            _client = Substitute.For<ImapClient>();
+            List<UniqueId> list = new List<UniqueId>();
+            UniqueId ui = new UniqueId(5);
+            list.Add(ui);
+            _client.Inbox.Search(SearchQuery.All).Returns(list);
+            // Act
+            List<Email> emails = service.GetMails("tom1.wales2@gmail.com", "Almafa1234");
+            // Assert
+            Assert.True(emails.Count != 0);
         }
     }
 }
